1a7b980e09ec7cdd675a025ead8373812e18749e
ckellis <cheyenne.kellis@gmail.com>
2015-10-30 17:34:41 -0700

    Beginning stages

--- server/schema.sql
+++ server/schema.sql
@@ -2,14 +2,34 @@ CREATE DATABASE chat;
 
 USE chat;
 
+CREATE TABLE rooms (
+  rID int NOT NULL AUTO_INCREMENT,
+  roomname varchar(20),
+  PRIMARY KEY (rID)
+);
+
+CREATE TABLE users (
+  uID int NOT NULL AUTO_INCREMENT,
+  username varchar(20),
+  PRIMARY KEY (uID)
+);
+
 CREATE TABLE messages (
   /* Describe your table here.*/
+  mID int NOT NULL AUTO_INCREMENT,
+  username varchar(20),
+  textMSG varchar(100),
+  rID int NOT NULL,
+  PRIMARY KEY (mID),
+  FOREIGN KEY (rID) REFERENCES rooms(rID)
 );
+
+
 
 /* Create other tables and define schemas for them here! */
 
 
-
+ 
 
 /*  Execute this file from the command line by typing:
  *    mysql -u root < server/schema.sql
